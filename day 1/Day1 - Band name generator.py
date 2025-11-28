e9a210dd7fc75002056855346276037513d09585
poribinees <aethertapie@gmail.com>
2025-11-28 22:47:45 +0900

    Create Day1 - Band name generator.py

---
+++ day 1/Day1 - Band name generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator")
+city = input("What city do you live in?\n")
+pet = input("What pet do you live in?\n")
+
+print("yo band name could be: " + city + " " + pet)
